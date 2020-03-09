760fbd14ff555e5edfd8dcfbb2b1cded3cf4675d
Ashton Meuser <ashtonmeuser@gmail.com>
2020-03-09 13:02:34 -0700

    Use environment vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh -l
 
-# Arguments:
-# 1. Token
-# 2. Namespace
-# 3. App name
-
 echo "$APP_NAME"
 
 cd /
@@ -13,6 +8,6 @@ oc login https://console.pathfinder.gov.bc.ca:8443 --token="$1" # Login to clust
 
 echo $(pwd)
 
-make create-server NAMESPACE="$2" APP_NAME="$3" REPO="https://github.com/$GITHUB_REPOSITORY" BRANCH=master IMAGE_TAG=latest
+make create-server NAMESPACE="$NAMESPACE" APP_NAME="$APP_NAME" REPO="https://github.com/$GITHUB_REPOSITORY" BRANCH=master IMAGE_TAG=latest
 
 # exit 1 # Ensure job fails so we can re-run GH Action
