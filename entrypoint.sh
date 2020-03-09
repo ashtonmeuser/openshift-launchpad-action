987ee4cc8982179424c5aa3e4e718668338e2300
Ashton Meuser <ashtonmeuser@gmail.com>
2020-03-09 09:11:01 -0700

    Tailor for Alpine

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 echo "$GITHUB_REPOSITORY"
 
 # OpenShift CLI
-wget --quiet -O oc.tar.gz https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-FILE=$(tar --warning=no-unknown-keyword -tf oc.tar.gz | grep '/oc$')
-tar --warning=no-unknown-keyword -zxf oc.tar.gz "$FILE"
-sudo mv "$FILE" /usr/local/bin/oc
+`wget --quiet -O oc.tar.gz https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz`
+FILE=$(tar -tf oc.tar.gz | grep '/oc$')
+tar -zxf oc.tar.gz "$FILE"
+mv "$FILE" /usr/local/bin/oc
 
 oc login https://console.pathfinder.gov.bc.ca:8443 --token="$1"
 
