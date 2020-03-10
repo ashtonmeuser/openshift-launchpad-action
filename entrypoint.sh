47e0c5aa1f5823767e92cf7a09248d5245765c2c
Ashton Meuser <ashtonmeuser@gmail.com>
2020-03-10 14:02:03 -0700

    Comments, added dockerignore, rename token variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/sh -l
 
+# Files from action repo copied to root dir on image creation
+# Files from consumer repo are located in $GITHUB_WORKSPACE
 cd /
 
-oc login https://console.pathfinder.gov.bc.ca:8443 --token="$OPENSHIFT_AUTH_TOKEN_TEST" # Login to cluster
+# Login to cluster
+oc login https://console.pathfinder.gov.bc.ca:8443 --token="$AUTH_TOKEN"
 
+# Clear previous builds including secrets & volume claims
 make oc-all-clean NAMESPACE=$NAMESPACE APP_NAME=$APP_NAME
 make oc-persisted-clean NAMESPACE=$NAMESPACE APP_NAME=$APP_NAME
 
+# Create network security policies, takes time to be applied
 make create-nsp NAMESPACE=$NAMESPACE APP_NAME=$APP_NAME
 sleep 30s
 
+# Run relevant make command, uses MODE input to select
 case "$MODE" in
   "server")
     make create-server NAMESPACE="$NAMESPACE" APP_NAME="$APP_NAME" REPO="https://github.com/$GITHUB_REPOSITORY" BRANCH=master IMAGE_TAG=latest
@@ -22,5 +28,3 @@ case "$MODE" in
     exit 1
     ;;
 esac
-
-exit 1 # Ensure job fails so we can re-run GH Action
